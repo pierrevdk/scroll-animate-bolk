e464560ec5cdf1765886a7e32971b84b6ba132a8
Pierre Vandekerckhove <pierre4vdk@gmail.com>
2021-04-08 20:16:38 +0200

    Update all images to remove first scene

--- scroll-animate-branding.js
+++ scroll-animate-branding.js
@@ -1,37 +1,14 @@
-// 1 to 151 => 00 - Sphere
-// 152 to 212 => 01 - Sphere to branding
-// 213 to 413 => 02 - Branding
-// 414 to 465  => 03 - Branding to website
-// 466 to 616 => 04 - Website
-// 617 to 757 => 05 - Website to contents
-// 758 to 908 => 06 - Contents
-// 909 to 958 => 07 - Contents to portfolio
+// 1 to 61 => 01 - Sphere to branding
+// 62 to 262 => 02 - Branding
+// 263 to 313  => 03 - Branding to website
+// 314 to 464 => 04 - Website
+// 465 to 655 => 05 - Website to contents
+// 656 to 806 => 06 - Contents
+// 807 to 856 => 07 - Contents to portfolio
 
-<<<<<<< HEAD
-// Global variable to control the scrolling behavior
-const step = 10; // For each 10px, change an image
-const section = document.getElementById("sticky-container");
-// const toto = window.scrollY;
-// console.log(toto.toString()); // => returns 0, why?
-
-function trackScrollPosition() {
-  const y = window.scrollY; // would like to put section.scrollTop here but does not work
-  const label = Math.min(Math.floor(y/10) + 1, 958); //Animate 200 images and change every 10 pixels
-  const imageToUse = [label];
-  // Change the background image
-  $('.image-container').css('background-image', `url('//res.cloudinary.com/bolk/image/upload/v1617375724/bolk-studio-website-js/scroll-animate/images/sphere-bolk-${imageToUse}.png')`); // TODO: Add url before ${}
-  console.log(y.toString());
-}
-
-$(document).ready(()=>{
-  $(window).scroll(()=>{
-    trackScrollPosition();
-  })
-})
-=======
 const containerID = 'sticky';
 const animationID = 'image-container';
-const frames = 958;
+const frames = 856;
 
 const { documentElement: page } = document;
 
@@ -80,4 +57,3 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
->>>>>>> 6e3867142062bd15f250c1635cd4ef7806acdacd
