e7f023c499df2b2658bbd9fd1866b07609ca3fe1
m4dz <code@m4dz.net>
2021-04-19 22:40:25 +0200

    Add preloading strategy

--- scroll-animate-services-bolk.js
+++ scroll-animate-services-bolk.js
@@ -6,25 +6,33 @@
 // 656 to 806 => 06 - Contents
 // 807 to 856 => 07 - Contents to portfolio
 
+const url = './images/sphere-bolk-{index}.png';
+
 const containerID = 'sticky';
 const animationID = 'image-container';
 const frames = 856;
+const fps = 60;
+
+const cache = new Map();
 
 const { documentElement: page } = document;
 
 document.addEventListener('DOMContentLoaded', () => {
+
   const container = document.getElementById(containerID);
   const animation = document.getElementById(animationID);
   const animCtx = animation.getContext('2d');
 
   let top = null;
   let isAnimated = false;
+  let dir = 1;
+  let cur = 0;
+  let nfr = 1;
 
   function animate () {
     const {
       top: currentTop,
-      height: currentHeight,
-      offsetTop
+      height: currentHeight
     } = container.getBoundingClientRect();
 
     isAnimated = (top == currentTop);
@@ -36,24 +44,71 @@ document.addEventListener('DOMContentLoaded', () => {
     const steps = timeline / frames;
     const distance = container.offsetTop;
 
-    const index = Math.min(Math.floor(distance / steps), frames);
-    draw(index);
+    let prev = dir;
 
+    nfr = Math.max(1, Math.min(Math.floor(distance / steps), frames));
+    dir = nfr > cur ? 1 : nfr < cur ? -1 : 0;
+
+    if (dir == 0 && prev != 0) {
+      preload(prev);
+    }
   }
 
-  function draw (index) {
+  window.addEventListener('scroll', () => {
+    window.requestAnimationFrame(animate);
+  });
+
+  function loader (index) {
+    return cache.has(index)
+    ? new Promise((resolve, reject) => { resolve(cache.get(index)) })
+    : fetch(url.replace('{index}', index))
+      .then(res => res.blob())
+      .then(blob => {
+        const src = URL.createObjectURL(blob);
+        cache.set(index, src);
+        return src;
+      });
+  }
+
+  function preload (prev, force) {
+    if (dir != 0 && !force) { return }
+
+    let limit = cur + fps * 1.5 * prev;
+
+    for (let i = cur + prev; i < limit; i = i + prev) {
+      const index = Math.max(1, Math.min(i, frames));
+      loader(index);
+    }
+  }
+
+  preload(dir, true);
+
+  function frame (src) {
     const loader = new Image();
     loader.addEventListener('load', () => {
       animCtx.clearRect(0, 0, animation.width, animation.height);
       animCtx.drawImage(loader, 0, 0);
     });
-    loader.src = `./images/sphere-bolk-${index}.png`;
+    loader.src = src;
   }
 
-  draw(1);
+  function draw () {
+    window.requestAnimationFrame(draw);
 
-  window.addEventListener('scroll', () => {
-    requestAnimationFrame(animate);
-  });
+    if (dir == 0 ||
+       (dir == 1 && cur >= nfr) ||
+       (dir == -1 && cur <= nfr )) { return }
+
+    let s = cur,
+        e = nfr,
+        f = 1 / (e - s);
+
+    do {
+      cur = cur + dir;
+      loader(cur).then(frame);
+    } while (cur != nfr)
+  }
+
+  draw();
 
 });
